5848acb63b674fd518932b291d01ddd5dbcb065e
TimM <t.makken@ocrn.nl>
2018-10-19 15:10:57 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+# README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='python37-openid',
+    version='0.1',
+    packages=['myapp'],
+    description='A line of description',
+    # long_description=README,
+    author='tim',
+    author_email='yourname@example.com',
+    url='https://github.com/timmakken/python37-openid.git',
+    license='MIT',
+    install_requires=[
+        'Django>=3.6',
+    ]
+)
